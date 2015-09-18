0975e5e464fe1659cf394f20440a6e3a32b3c992
amirsad2 <deadamirbot@gmail.com>
2015-09-18 15:26:25 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -203,6 +203,7 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
+      "tex",
       "webshot",
       "addgp",
       "echo",
