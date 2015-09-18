b73f55a0b6dfbb4b2a3c426d42c4320860f46582
amirsad2 <deadamirbot@gmail.com>
2015-09-18 15:14:12 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -203,7 +203,7 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-      "add",
+      "addgp",
       "echo",
       "get",
       "google",
