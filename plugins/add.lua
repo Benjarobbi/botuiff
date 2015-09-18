b7cf9da01f7063f6e59c7617ce1d8d56d90ed303
amirsad2 <deadamirbot@gmail.com>
2015-09-18 15:13:41 +0430

    Update add.lua

--- plugins/add.lua
+++ plugins/add.lua
@@ -1,21 +1,17 @@
--do
--
--function run(msg, matches)
-- send_contact(get_receiver(msg), "+639080735608", "@Ugly", "Bot", ok_cb, false)
--end
--
--return {
--  description = "Send bot number to add", 
--  usage = "!add",
--  patterns = {
--    "^!add$"
--
--
--
--
--
--  }, 
--  run = run 
--}
--
--end
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+639080735608", "ugly", "Bot", ok_cb, false)
+end
+
+return {
+description = "contact for add to gp",
+usage = "contact",
+patterns = {
+"^!addgp$"
+
+},
+run = run
+}
+
+end
